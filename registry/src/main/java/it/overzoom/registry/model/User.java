d185017403f4077d2c38e9a17b4def994a8fc920
Flavio Troia <flavio.troia@overzoom.it>
2025-02-13 11:51:14 +0100

    fix: add user type

--- registry/src/main/java/it/overzoom/registry/model/User.java
+++ registry/src/main/java/it/overzoom/registry/model/User.java
@@ -6,6 +6,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Document(collection = "user")
@@ -25,4 +26,7 @@ public class User {
     private List<String> customers;
 
     private List<String> roles;
+
+    @NotNull
+    private UserType type;
 }
---
+++ registry/src/main/java/it/overzoom/registry/model/UserType.java
@@ -0,0 +1,6 @@
+package it.overzoom.registry.model;
+
+public enum UserType {
+    EQ,
+    CUSTOMER
+}
