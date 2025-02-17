4ead309ec5b1cd996599e960b3c310c8a94fbdd4
FlavioTroja <flavio.troia@overzoom.it>
2025-02-17 18:34:16 +0100

    fix: add keycloak

---
+++ build.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Funzione per il logging con timestamp
+log() {
+  echo "[$(date +"%Y-%m-%d %H:%M:%S")] $1"
+}
+
+# Array dei nomi delle cartelle dei microservizi
+services=("registry" "gateway" "eurekaserver" "calendar")
+
+for service in "${services[@]}"; do
+  if [ -d "$service" ]; then
+    log "La cartella '$service' è presente. Avvio build..."
+    cd "$service" || { log "Impossibile entrare nella cartella $service."; exit 1; }
+    
+    # Verifica che lo script gradlew esista
+    if [ -f "gradlew" ]; then
+      sh gradlew bootJar
+      if [ $? -eq 0 ]; then
+        log "Build completata con successo per '$service'."
+      else
+        log "Errore durante la build per '$service'."
+      fi
+    else
+      log "Il file 'gradlew' non è stato trovato in '$service'."
+    fi
+    cd ..
+  else
+    log "La cartella '$service' non è presente. Skip build."
+  fi
+done
+
+log "Costruisco i container Docker..."
+docker compose build
+if [ $? -eq 0 ]; then
+  log "Build dei container Docker completata con successo."
+else
+  log "Errore durante la build dei container Docker."
+fi
+
+log "Avvio dei container Docker in modalità detached..."
+docker compose up -d
+if [ $? -eq 0 ]; then
+  log "I container Docker sono stati avviati correttamente."
+else
+  log "Errore nell'avvio dei container Docker."
+fi
+
