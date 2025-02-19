4b9357a7630cb0faf71eb812637fbbd07e730376
ABelli99 <andreabelli1999@gmail.com>
2025-02-19 12:36:31 +0100

    [JAVA] Fix Java version

--- EQ-FE/src/app/core/ui/services/sidebar.routes.ts
+++ EQ-FE/src/app/core/ui/services/sidebar.routes.ts
@@ -19,46 +19,18 @@ export const sidebarRoutes: RouteElement[] = [
     isLast: true,
   },
   {
-    iconName: "package_2",
-    path: "/products",
-    label: "Prodotti",
-    children: [
-      { iconName: "package_2", label: "Lista Prodotti", path: "/products" },
-      { iconName: "category", label: "Categorie", path: "/categories" }
-    ]
+    iconName: "description",
+    path: "/verbals",
+    label: "Verbali",
   },
   {
-    iconName: "open_with",
-    path: "/cargos",
-    label: "Movimentazioni"
-  },
-  {
-    iconName: "local_shipping",
-    path: "/suppliers",
-    label: "Fornitori",
-    roleSelector: "sidebar.body.suppliers"
+    iconName: "precision_manufacturing",
+    path: "/machines",
+    label: "Macchine"
   },
   {
     iconName: "group",
     path: "/customers",
     label: "Clienti",
-  },
-  {
-    iconName: "person",
-    path: "/resources",
-    label: "Risorse",
-    roleSelector: "sidebar.body.resources",
-    isLast: true,
-  },
-  {
-    iconName: "road",
-    path: "/inspections",
-    label: "Allestimenti",
-    children: [
-      { iconName: "category_search", label: "Sopralluoghi", path: "/inspections" },
-      { iconName: "assignment", label: "Schede tecniche", path: "/taskSteps" },
-      { iconName: "service_toolbox", label: "Lavori avviati", path: "/inspections/new/view" },
-      { iconName: "work_history", label: "Lavori conclusi", path: "/inspections/new/view" },
-    ]
   },
 ];
