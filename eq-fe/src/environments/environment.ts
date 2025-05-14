968d59d94660ca0c523898a6fbb0c72c20495059
Flavio Troia <flavio.troia@overzoom.it>
2025-05-14 10:30:52 +0200

    fix: remove pre-commit

--- eq-fe/src/environments/environment.ts
+++ eq-fe/src/environments/environment.ts
@@ -1,5 +1,6 @@
+
 export const environment = {
   production: false,
   appVersion: require('../../package.json').version,
-  BASE_URL: "https://effeti.overzoom.it/api"
+  BASE_URL: "http://localhost:8765",
 };
