453387ab24caca97c5fb3d968ce9721ea2f29305
Flavio Troia <flavio.troia@overzoom.it>
2025-05-27 11:51:40 +0200

    fix: cors

--- gateway/src/main/java/it/overzoom/gateway/config/CorsConfig.java
+++ gateway/src/main/java/it/overzoom/gateway/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        config.setAllowedHeaders(List.of("*"));
+        config.setAllowedOriginPatterns(List.of("https://radpro.it", "http://localhost:4200"));
         config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
